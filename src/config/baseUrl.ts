2b5a7dde2e38974da5a239e2407be6e2d2b9c33c
Andrey Soares <andreysoares.dev@gmail.com>
2025-03-28 17:26:21 -0300

    fix: base endpoint

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/api"
-    : "https://chatbot-backend-gs7y.onrender.com";
+    : "https://chatbot-backend-gs7y.onrender.com/api";
