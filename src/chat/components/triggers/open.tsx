68e70ce64c625e356c00bcc42a083b95c40e7fcd
Andrey Soares <andreysoares.dev@gmail.com>
2025-03-28 17:28:00 -0300

    style: fix trigger position

--- src/chat/components/triggers/open.tsx
+++ src/chat/components/triggers/open.tsx
@@ -86,8 +86,8 @@ const NativeOpen = () => {
       className={clsx({
         "hover:-translate-y-1 stroke-secondary hover:stroke-primary": true,
         hidden: chatbot.visible,
-        "absolute bottom-5 left-5": props?.triggerPosition === "left",
-        "absolute bottom-5 right-5": props?.triggerPosition === "right",
+        "fixed bottom-5 left-5": props?.triggerPosition === "left",
+        "fixed bottom-5 right-5": props?.triggerPosition === "right",
       })}
       onClick={() => updateChat({ visible: true })}
     >
